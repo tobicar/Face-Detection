c9dfba85e953f7b2b3b8d65b622ac7807618c7fe
Svea Worms <svea@worms-web.de>
2022-11-09 14:11:38 +0100

    updated TrainingScratch.py, TrainingTransfer.py train versions in loop

--- TrainingScratch.py
+++ TrainingScratch.py
@@ -1,30 +1,28 @@
 ## imports
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-import datetime
 import MobileNet
 
-#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 # Trainingspipeline:
-BATCH_SIZE = 64 #TODO: Hier varieren mit 32, 64, 128, 256
-EPOCHS = 50 #TODO: Hier variieren
+BATCH_SIZES = [32, 64, 128, 256]
+# BATCH_SIZE = 64  # TODO: Hier varieren mit 32, 64, 128, 256
+EPOCHS = [10, 30, 50, 100]
 
-#TODO: Crossvalidation
-#TODO: Learning Rate Decay ???
-#TODO: Early Stopping (Overfitting vermeiden)
+# TODO: Crossvalidation
+# TODO: Learning Rate Decay ???
+# TODO: Early Stopping (Overfitting vermeiden)
 
-## load train and validation data
-(train_ds, val_ds) = MobileNet.import_train_images("images/train", batch_size=BATCH_SIZE)
-## buffer datasets in RAM to prevent I/0 Blocking
-AUTOTUNE = tf.data.AUTOTUNE
-train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
-## load net architecture
-model_scratch = MobileNet.load_model_for_training("v3Large", 1) #TODO: Dropout: 0.1 0.2 0.3
-
-## print summary of the model
-model_scratch.summary()
-## load,train and save model
-history = MobileNet.train_model(model_scratch, EPOCHS, train_ds, val_ds, "model_scratch_" + str(EPOCHS) + "epochs_"
-                                + str(BATCH_SIZE) + "batch")
+##
+for batch in BATCH_SIZES:
+    for epoch in EPOCHS:
+        # load train and validation data
+        (train_ds, val_ds) = MobileNet.import_train_images("images/train", batch_size=batch)
+        # buffer datasets in RAM to prevent I/0 Blocking
+        AUTOTUNE = tf.data.AUTOTUNE
+        train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+        val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+        # load net architecture
+        model_scratch = MobileNet.load_model_for_training("v3Large", 1)  # TODO: Dropout: 0.1 0.2 0.3
+        # load,train and save model
+        name = "model_scratch_" + str(epoch) + "epochs_" + str(batch) + "batch"
+        history = MobileNet.train_model(model_scratch, epoch, train_ds, val_ds, name)
+        MobileNet.generate_history_and_save(history, name)
