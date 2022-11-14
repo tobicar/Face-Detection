4f258d9d9f1b58f4829d195e537e7f1ce476ec09
Tobias Kaps <tobias.kaps@gmx.de>
2022-11-14 15:48:30 +0100

    evaluation.py: automatisierte evaluierung über alle Modelle in saved_models hinzugefügt

--- evaluation.py
+++ evaluation.py
@@ -1,24 +1,40 @@
 ## imports
+import pandas as pd
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
+from datetime import date
 import MobileNet
+import os
+## load test dataset with batchsize 1
+test_ds = MobileNet.import_test_images("images/test",1)
+## evaluate all current models and save name, loss and acuraccy to array
+data = []
+directory = "saved_model"
+for model_path in os.listdir(directory):
+    # because of MAC OS DS_Store folder
+    if model_path == ".DS_Store":
+        continue
+    model = tf.keras.models.load_model(directory + "/" +model_path)
+    evaluation = model.evaluate(test_ds)
+    row = {"name": model_path, "loss": evaluation[0], "acc": evaluation[1]}
+    data.append(row)
+## save data to pandas Dataframe and to file
+df = pd.DataFrame(data)
+time = datetime.now().strftime("%m:%d:%Y:%H:%M:%S")
+df.to_csv("evaluation/" + str(time) + ".csv")
 
-## load model
+
+
+## load one model (old)
 PATH_TO_MODEL = "saved_model/model_transfer_10epochs_32batch"
 model = tf.keras.models.load_model(PATH_TO_MODEL)
-## load test Dataset
-test_ds = MobileNet.import_test_images("images/test")
-
-## evaluierung des Models
-#TODO: Implementierung und model.evaluate anschauen
-evaluation = model.evaluate(test_ds)
 ##
-#TODO: Visualisierung der Dateb
+#TODO: Visualisierung der Daten
 
 ##
-## OpenFileDialog
+## OpenFileDialog and predict one Image
 
 import tkinter as tk
 from tkinter import filedialog
