257406918927933e2652011a5e300987f1a9355b
Svea Worms <svea@worms-web.de>
2022-11-16 19:52:28 +0100

    added model scratch with 10 and 30 epochs

--- TrainingScratch.py
+++ TrainingScratch.py
@@ -3,14 +3,12 @@ import tensorflow as tf
 import MobileNet
 
 # train pipeline:
-BATCH_SIZES = [32]
-EPOCHS = [75, 30]
-
-ALPHAS = [1, 0.75, 0.5, 0.25, 1.25, 1.5, 1.75, 2]
-
-DEPTH_MULTIPLIERS = [1, 0.75, 0.5]  # ERROR: scheint nicht richtig implementiert zu sein in MobileNet
-
-INPUT_SIZES = [192, 160, 128]
+BATCH_SIZES = [64]
+EPOCHS = [30, 10]
+ALPHAS = [1]  # [1, 0.75, 0.5, 0.25, 1.25]
+# DEPTH_MULTIPLIERS = [1, 0.75, 0.5]  # ERROR: scheint nicht richtig implementiert zu sein in MobileNet
+INPUT_SIZES = [224]  # [192, 160, 128]
+FULL_NAME = False
 
 # TODO: Cross-validation
 # TODO: Learning Rate Decay ???
@@ -20,29 +18,27 @@ INPUT_SIZES = [192, 160, 128]
 alpha = 1.0
 for batch in BATCH_SIZES:
     for epoch in EPOCHS:
-        for size in INPUT_SIZES:
-            #for depth_multiplier in DEPTH_MULTIPLIERS:
-            #if depth_multiplier == 1 and alpha == 1:
-            #   continue
-            #print("alpha: " + str(alpha) + "\ndepth multiplier: " + str(depth_multiplier))
-            # load train and validation data
-            (train_ds, val_ds) = MobileNet.import_train_images("images/train", batch_size=batch, imagesize=size)
-            # buffer datasets in RAM to prevent I/0 Blocking
-            AUTOTUNE = tf.data.AUTOTUNE
-            train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
-            val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
-            # load net architecture
-            model_scratch = MobileNet.load_model_for_training("v1", 1, input_size=size)
-            # load,train and save model
-            name = "modelv1_scratch_" + str(epoch) + "epochs_" + str(batch) + "batch_" + str(alpha).split(".")[0] +\
-                   str(alpha).split(".")[1] + "alpha_" + str(size) + "inputSize"
-            history = MobileNet.train_model(model_scratch, epoch, train_ds, val_ds, name)
-            MobileNet.generate_history_and_save(history, name)
+        for alpha in ALPHAS:
+            for size in INPUT_SIZES:
+                # load train and validation data
+                (train_ds, val_ds) = MobileNet.import_train_images("images/train", batch_size=batch, imagesize=size)
+                # buffer datasets in RAM to prevent I/0 Blocking
+                AUTOTUNE = tf.data.AUTOTUNE
+                train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+                val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+                # load net architecture
+                model_scratch = MobileNet.load_model_for_training("v1", 1)
+                # load,train and save model
+                name = "modelv1_scratch_" + str(epoch) + "epochs_" + str(batch) + "batch"
+                if FULL_NAME:
+                    name += "_" + str(alpha).split(".")[0] + str(alpha).split(".")[1] + "alpha_" + str(size) + "inputSize"
+                history = MobileNet.train_model(model_scratch, epoch, train_ds, val_ds, name)
+                MobileNet.generate_history_and_save(history, name)
 
 ##
 # train on ResNet50
 
-#for batch in BATCH_SIZES:
+# for batch in BATCH_SIZES:
 #    for epoch in [30, 50]:
 #        # load train and validation data
 #        (train_ds, val_ds) = MobileNet.import_train_images("images/train", batch_size=batch)
