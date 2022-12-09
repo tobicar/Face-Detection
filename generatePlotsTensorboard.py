e5b8f8e44291792a8d96f5812a30eaa2ec04cdf8
svea <svea@worms-web.de>
2022-12-09 09:11:38 +0100

    added tensorboard csv files for presentation

--- generatePlotsTensorboard.py
+++ generatePlotsTensorboard.py
@@ -3,22 +3,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 ## modelv1_transfer_30epochs
-train_acc = pd.read_csv("presentation/modelv1_transfer_30epochs/32batches/run-20221116-082410_train-tag-epoch_accuracy.csv")
-val_acc = pd.read_csv("presentation/modelv1_transfer_30epochs/32batches/run-20221116-082410_validation-tag-epoch_accuracy.csv")
-train_acc_64 = pd.read_csv("presentation/modelv1_transfer_30epochs/64batches/run-20221116-083644_train-tag-epoch_accuracy.csv")
-val_acc_64 = pd.read_csv("presentation/modelv1_transfer_30epochs/64batches/run-20221116-083644_validation-tag-epoch_accuracy.csv")
-train_acc_128 = pd.read_csv("presentation/modelv1_transfer_30epochs/128batches/run-20221116-085356_train-tag-epoch_accuracy.csv")
-val_acc_128 = pd.read_csv("presentation/modelv1_transfer_30epochs/128batches/run-20221116-085356_validation-tag-epoch_accuracy.csv")
-train_acc_256 = pd.read_csv("presentation/modelv1_transfer_30epochs/256batches/run-20221116-090730_train-tag-epoch_accuracy.csv")
-val_acc_256 = pd.read_csv("presentation/modelv1_transfer_30epochs/256batches/run-20221116-090730_validation-tag-epoch_accuracy.csv")
-train_loss = pd.read_csv("presentation/modelv1_transfer_30epochs/32batches/run-20221116-082410_train-tag-epoch_loss.csv")
-val_loss = pd.read_csv("presentation/modelv1_transfer_30epochs/32batches/run-20221116-082410_validation-tag-epoch_loss.csv")
-train_loss_64 = pd.read_csv("presentation/modelv1_transfer_30epochs/64batches/run-20221116-083644_train-tag-epoch_loss.csv")
-val_loss_64 = pd.read_csv("presentation/modelv1_transfer_30epochs/64batches/run-20221116-083644_validation-tag-epoch_loss.csv")
-train_loss_128 = pd.read_csv("presentation/modelv1_transfer_30epochs/128batches/run-20221116-085356_train-tag-epoch_loss.csv")
-val_loss_128 = pd.read_csv("presentation/modelv1_transfer_30epochs/128batches/run-20221116-085356_validation-tag-epoch_loss.csv")
-train_loss_256 = pd.read_csv("presentation/modelv1_transfer_30epochs/256batches/run-20221116-090730_train-tag-epoch_loss.csv")
-val_loss_256 = pd.read_csv("presentation/modelv1_transfer_30epochs/256batches/run-20221116-090730_validation-tag-epoch_loss.csv")
+train_acc = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/32batches/run-20221116-082410_train-tag-epoch_accuracy.csv")
+val_acc = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/32batches/run-20221116-082410_validation-tag-epoch_accuracy.csv")
+train_acc_64 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/64batches/run-20221116-083644_train-tag-epoch_accuracy.csv")
+val_acc_64 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/64batches/run-20221116-083644_validation-tag-epoch_accuracy.csv")
+train_acc_128 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/128batches/run-20221116-085356_train-tag-epoch_accuracy.csv")
+val_acc_128 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/128batches/run-20221116-085356_validation-tag-epoch_accuracy.csv")
+train_acc_256 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/256batches/run-20221116-090730_train-tag-epoch_accuracy.csv")
+val_acc_256 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/256batches/run-20221116-090730_validation-tag-epoch_accuracy.csv")
+train_loss = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/32batches/run-20221116-082410_train-tag-epoch_loss.csv")
+val_loss = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/32batches/run-20221116-082410_validation-tag-epoch_loss.csv")
+train_loss_64 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/64batches/run-20221116-083644_train-tag-epoch_loss.csv")
+val_loss_64 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/64batches/run-20221116-083644_validation-tag-epoch_loss.csv")
+train_loss_128 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/128batches/run-20221116-085356_train-tag-epoch_loss.csv")
+val_loss_128 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/128batches/run-20221116-085356_validation-tag-epoch_loss.csv")
+train_loss_256 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/256batches/run-20221116-090730_train-tag-epoch_loss.csv")
+val_loss_256 = pd.read_csv(
+    "presentation/milestone2/modelv1_transfer_30epochs/256batches/run-20221116-090730_validation-tag-epoch_loss.csv")
 ##
 fig, ax = plt.subplots()
 ax.plot(train_acc_256["Step"], train_acc_256["Value"], label="training")
@@ -44,14 +60,22 @@ plt.savefig("presentation/modelv1_transfer_30epochs_256batches_run-20221116-0907
 
 
 ## modelv1_scratch_100epochs
-train_acc = pd.read_csv("presentation/modelv1_scratch_100epochs/32batches/run-20221115-200829_train-tag-epoch_accuracy.csv")
-val_acc = pd.read_csv("presentation/modelv1_scratch_100epochs/32batches/run-20221115-200829_validation-tag-epoch_accuracy.csv")
-train_acc_64 = pd.read_csv("presentation/modelv1_scratch_100epochs/64batches/run-20221115-212035_train-tag-epoch_accuracy.csv")
-val_acc_64 = pd.read_csv("presentation/modelv1_scratch_100epochs/64batches/run-20221115-212035_validation-tag-epoch_accuracy.csv")
-train_loss = pd.read_csv("presentation/modelv1_scratch_100epochs/32batches/run-20221115-200829_train-tag-epoch_loss.csv")
-val_loss = pd.read_csv("presentation/modelv1_scratch_100epochs/32batches/run-20221115-200829_validation-tag-epoch_loss.csv")
-train_loss_64 = pd.read_csv("presentation/modelv1_scratch_100epochs/64batches/run-20221115-212035_train-tag-epoch_loss.csv")
-val_loss_64 = pd.read_csv("presentation/modelv1_scratch_100epochs/64batches/run-20221115-212035_validation-tag-epoch_loss.csv")
+train_acc = pd.read_csv(
+    "presentation/milestone2/modelv1_scratch_100epochs/32batches/run-20221115-200829_train-tag-epoch_accuracy.csv")
+val_acc = pd.read_csv(
+    "presentation/milestone2/modelv1_scratch_100epochs/32batches/run-20221115-200829_validation-tag-epoch_accuracy.csv")
+train_acc_64 = pd.read_csv(
+    "presentation/milestone2/modelv1_scratch_100epochs/64batches/run-20221115-212035_train-tag-epoch_accuracy.csv")
+val_acc_64 = pd.read_csv(
+    "presentation/milestone2/modelv1_scratch_100epochs/64batches/run-20221115-212035_validation-tag-epoch_accuracy.csv")
+train_loss = pd.read_csv(
+    "presentation/milestone2/modelv1_scratch_100epochs/32batches/run-20221115-200829_train-tag-epoch_loss.csv")
+val_loss = pd.read_csv(
+    "presentation/milestone2/modelv1_scratch_100epochs/32batches/run-20221115-200829_validation-tag-epoch_loss.csv")
+train_loss_64 = pd.read_csv(
+    "presentation/milestone2/modelv1_scratch_100epochs/64batches/run-20221115-212035_train-tag-epoch_loss.csv")
+val_loss_64 = pd.read_csv(
+    "presentation/milestone2/modelv1_scratch_100epochs/64batches/run-20221115-212035_validation-tag-epoch_loss.csv")
 
 ##
 fig, ax = plt.subplots()
