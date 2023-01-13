cbbad3df0783356ab69136d9487c1e9243103f6c
Svea Worms <svea@worms-web.de>
2023-01-13 11:31:37 +0100

    changed predictMilestone4.py plot

--- predictMilestone4.py
+++ predictMilestone4.py
@@ -88,10 +88,12 @@ database_list = np.array(database_list)
 
 ## random generated database
 #images_db, database_list = generate_database("/Users/tobias/PycharmProjects/Face-Detection/images/rawdata4/archive/Faces/Faces", 10)
-images_db, database_list = generate_database("C:\\Users\\Svea Worms\\PycharmProjects\\Face-Detection\\images\\rawdata4", 5)
+images_db, database_list = generate_database("C:\\Users\\Svea Worms\\PycharmProjects\\Face-Detection\\images\\milestone4\\train", 5)
 ## choose image to predict
 #IMG_TO_PREDICT_PATH = "/Users/tobias/PycharmProjects/Face-Detection/images/rawdata4/archive/Faces/Faces/Zac Efron_4.jpg"
-IMG_TO_PREDICT_PATH = r"C:\Users\Svea Worms\PycharmProjects\Face-Detection\images\milestone4\test\Ellen Degeneres_55.jpg"
+#IMG_TO_PREDICT_PATH = r"C:\Users\Svea Worms\Downloads\predictions\mensch1.jpg"
+IMG_TO_PREDICT_PATH = r"C:\Users\Svea Worms\Downloads\predictions\31_0_0_20170104201726242.jpg.chip.jpg"
+name = "Tom Cruise"
 img = decode_image(IMG_TO_PREDICT_PATH)
 image_list = np.array([img]*len(images_db))
 image_list_1 = image_list[0:int(len(image_list)*0.5)]
@@ -104,6 +106,8 @@ TRIPLET_LOSS_MODEL = True
 if TRIPLET_LOSS_MODEL:
     prediction_1 = siamese_model.predict([image_list_1, database_list_1, database_list_1])
     prediction_2 = siamese_model.predict([image_list_2, database_list_2, database_list_2])
+    #prediction_1 = siamese_model.predict([database_list_1, image_list_1, image_list_1])
+    #prediction_2 = siamese_model.predict([database_list_2, image_list_2, image_list_2])
     prediction = np.append(prediction_1[0], prediction_2[0])
     images_db["pred"] = prediction
 else:
@@ -117,9 +121,13 @@ top4 = pred_class.nsmallest(3)
 fig, ax = plt.subplots()
 ax.imshow(tf.keras.preprocessing.image.array_to_img(img))
 textstr = '\n'.join((
-                '1. \n mean: %s: %.2f \n min:  %s: %.2f' % (top4.index[0], top4[0], top4_min.index[0], top4_min[0]),
-                '2. \n mean: %s: %.2f \n min: %s: %.2f' % (top4.index[1], top4[1], top4_min.index[1], top4_min[1]),
-                '3. \n mean: %s: %.2f \n min: %s: %.2f' % (top4.index[2], top4[2], top4_min.index[2], top4_min[2])
+                '1. %s: %.2f' % (top4_min.index[0], top4_min[0]),
+                '2. %s: %.2f' % (top4_min.index[1], top4_min[1]),
+                '3. %s: %.2f' % (top4_min.index[2], top4_min[2]),
+                '\n real: %s' % (name)
+                #'1. \n mean: %s: %.2f \n min:  %s: %.2f' % (top4.index[0], top4[0], top4_min.index[0], top4_min[0]),
+                #'2. \n mean: %s: %.2f \n min: %s: %.2f' % (top4.index[1], top4[1], top4_min.index[1], top4_min[1]),
+                #'3. \n mean: %s: %.2f \n min: %s: %.2f' % (top4.index[2], top4[2], top4_min.index[2], top4_min[2])
 ))
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 ax.text(240, 112, textstr, fontsize=14, bbox=props, verticalalignment="center")
@@ -142,32 +150,23 @@ img2 = tf.expand_dims(img2, axis=0)
 prediction = model.predict([img1, img2])
 print(prediction)
 ##
-def evaluateTestset(threshold, images_db, database_list, path_to_testset, mac_os, siamese_model):
+def evaluateTestsetTripletLoss(threshold, images_db, database_list, path_to_testset, mac_os, siamese_model):
     if mac_os:
         images = sorted([(str(path_to_testset + "/" + f), str(f.split("_")[0])) for f in os.listdir(path_to_testset)])
     else:
         images = sorted([(str(path_to_testset + "\\" + f), str(f.split("_")[0])) for f in os.listdir(path_to_testset)])
 
     images_test = pd.DataFrame(images, columns=["path", "class"])
-    #pred = []
     pred_sum = 0
     for index, image in images_test.iterrows():
         img = decode_image(image["path"])
         image_list = np.array([img] * len(images_db))
         prediction = siamese_model.predict([image_list, database_list, database_list])
         images_db["pred"] = prediction[0]
-        print("\n -----1------------------------- \n")
-        #pred_class = images_db.groupby("class").apply(lambda x: x['pred'].sum() / len(x))
         pred_class_min = images_db.groupby("class").min()["pred"]
-        print("\n -----2------------------------- \n")
         top1_min = pred_class_min.nsmallest(1)
-        print("\n -----3------------------------- \n")
         if top1_min[0] <= threshold and top1_min.index[0] == image["class"]:
-            #pred.append([1])
             pred_sum += 1
-            print("\n -----4------------------------- \n")
-        #else:
-            #pred.append([0])
 
     accuracy = pred_sum/images_test["path"].count()
     return accuracy
@@ -186,18 +185,23 @@ def evaluateTestsetContrastiveLoss(threshold, images_db, database_list, path_to_
         image_list = np.array([img] * len(images_db))
         prediction = model.predict([image_list, database_list])
         images_db["pred"] = prediction
-        print("\n -----1------------------------- \n")
         # pred_class = images_db.groupby("class").apply(lambda x: x['pred'].sum() / len(x))
         pred_class_min = images_db.groupby("class").min()["pred"]
-        print("\n -----2------------------------- \n")
         top1_min = pred_class_min.nsmallest(1)
-        print("\n -----3------------------------- \n")
         if top1_min[0] <= threshold and top1_min.index[0] == image["class"]:
-            # pred.append([1])
             pred_sum += 1
-            print("\n -----4------------------------- \n")
-        # else:
-        # pred.append([0])
 
     accuracy = pred_sum / images_test["path"].count()
     return accuracy
+
+##
+
+predictions = []
+for t in [0.2, 0.4, 0.6, 0.8]:
+    #pred = evaluateTestsetContrastiveLoss(t, images_db, database_list, r"C:\Users\Svea Worms\PycharmProjects\Face-Detection\images\milestone4\test", False, model)
+    pred = evaluateTestsetTripletLoss(t, images_db, database_list, r"C:\Users\Svea Worms\PycharmProjects\Face-Detection\images\milestone4\test", False, siamese_model)
+    predictions.append([t, pred])
+
+
+print(predictions)
+
